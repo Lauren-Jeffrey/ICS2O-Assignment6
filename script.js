0005885c6b759bbab28a62ae8776d7c7e3e3d0d5
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-13 15:59:40 +0000

    js

--- script.js
+++ script.js
@@ -3,26 +3,20 @@ function myfunction () {
   let size = document.getElementById('pizzasize').value
   if (size === 'Large') {
     size = 6.00
-  }
-  else if (size === 'Extra Large') {
+  } else if (size === 'Extra Large') {
     size = 10.00
   }
   let topping = document.getElementById('toppingnum').value
   if (topping === '1') {
-    topping = 1.00;
+    topping = 1.00
+  } else if (topping === '2') {
+    topping = 1.75
+  } else if (topping === '3') {
+    topping = 2.50
+  } else if (topping === '4') {
+    topping = 3.35
   }
-  else if (topping === '2') {
-    topping = 1.75;
-  }
-  else if (topping === '3') {
-    topping = 2.50;
-  }
-  else if (topping === '4') {
-    topping = 3.35;
-  }
-  let total
-  let cost
-  total = size + topping;
-  cost = total * 1.13
+  const total = size + topping
+  const cost = total * 1.13
   alert('The total cost (with taxes) is $' + cost)
 }
