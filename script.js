05e7ce2d3ca707fabb3cad3c5ce6ffb937d8b5fd
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2021-01-13 15:20:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.getElementById('price').addEventListener('click', myfunction)
+function myfunction () {
+  let size = document.getElementById('pizzasize').value
+  if (size == 'Large') {
+    size = 6.00;
+  }
+  else if (size == 'Extra Large') {
+    size = 10.00;
+  }
+  let topping = document.getElementById('toppingnum').value
+  if (topping == '1') {
+    topping = 1.00;
+  }
+  else if (topping == '2') {
+    topping = 1.75;
+  }
+  else if (topping == '3') {
+    topping = 2.50;
+  }
+  else if (topping == '4') {
+    topping = 3.35;
+  }
+  let total
+  let cost
+  total = size + topping;
+  cost = total * 1.13
+  alert('The total cost (with taxes) is $'+cost)
+}
